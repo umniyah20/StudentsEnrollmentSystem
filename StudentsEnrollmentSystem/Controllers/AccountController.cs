e22197889b80736afcc4098c54a2194772d18945
UMNIYAH <UMNIYAHJUMAAH.swi@GMAIL.COM>
2025-07-05 16:17:12 +0300

    #FRONT END TAMAM

--- StudentsEnrollmentSystem/Controllers/AccountController.cs
+++ StudentsEnrollmentSystem/Controllers/AccountController.cs
@@ -3,5 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 using StudentsEnrollmentSystem.ViewModels;
 public class AccountController : Controller
 {
-   
+   public IActionResult login()
+    {
+        return View();
+    }
+
+
+    public IActionResult Signin()
+    {
+        return View();
+    }
+
+    public IActionResult ForgotPass()
+    {
+        return View();
+    }
+
+
 }
